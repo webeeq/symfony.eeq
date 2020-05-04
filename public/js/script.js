4c2501da0f4245a076469b79e92b109fa1045f83
Robert Rybiański <40652170+webeeq@users.noreply.github.com>
2020-05-04 09:24:05 +0200

    Change ajax code

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,3 @@ $(document).ready(function() {
 
     $("#frame").css("height", (document.documentElement.clientHeight - 155) + "px");
 });
-
-function ajaxData(id, file) {
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById(id).innerHTML = xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", file, true);
-    xmlhttp.send();
-}
