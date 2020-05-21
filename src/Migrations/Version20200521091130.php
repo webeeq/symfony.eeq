177bbf770440e3a773d7ed5b32cdd0aa4549ee09
Robert Rybiański <40652170+webeeq@users.noreply.github.com>
2020-05-21 11:20:25 +0200

    Change migration files

--- src/Migrations/Version20200521091130.php
+++ src/Migrations/Version20200521091130.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200326095423 extends AbstractMigration
+final class Version20200521091130 extends AbstractMigration
 {
     public function getDescription() : string
     {
